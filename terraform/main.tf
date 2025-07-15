84660a96a5a72f01619eb362b759ecb2276481aa
Srushti Deshmukh <srushti.yourmail@example.com>
2025-07-15 11:01:46 +0530

    Final: added execution_role_arn to ECS task definition

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,6 +48,7 @@ resource "aws_ecs_task_definition" "medusa" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -75,4 +76,25 @@ resource "aws_ecs_service" "medusa" {
   }
 
   desired_count = 1
+}
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
